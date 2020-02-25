79b16bb2934d79a0ad635a7fe5f233e83fb0187d
Kelly Moulds <kellyamoulds@gmail.com>
2020-02-24 21:03:33 -0800

    nav bar edits

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
 $(() => {
 
-
-
     $(window).on('load', () => {
         $("body").addClass('all-loaded');
     });
+
+    $(document).ready(function() {
+        $(this).scrollTop(0);
+    });
+
 });
